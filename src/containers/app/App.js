0c21cde4aa5f7a506c0cc2470ad48e7b3d05ca32
Tom <tomaslm_3@hotmail.co>
2022-03-02 11:58:48 -0300

    Added component Carousel features

--- src/containers/app/App.js
+++ src/containers/app/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import CarouselFeatures from '../../components/carrouselFeatures/CarouselFeatures';
+import { carrouselData } from '../../components/carrouselFeatures/carouselData';
 
 function App() {
   return (
     <div className="App">
-
+      <CarouselFeatures slides={carrouselData}/>
     </div>
   );
 }
