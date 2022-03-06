75929b0b34daa81ecfd119ca13d8303f80238d8e
Ignacio Castillo <ignacaas@gmail.com>
2022-03-06 13:41:29 -0300

    Creacion NavBar

---
+++ src/components/NavBar/MenuItems/MenuItems.js
@@ -0,0 +1,19 @@
+
+import React from 'react'
+import './MenuItems.css'
+
+const MenuItems = () => {
+  return (
+ <>
+ <ul className='menu-items'>
+ <li><a href='#'>INICIO</a></li>
+ <li><a href='#'>CATALOGO</a></li>
+ <li><a href='#'>OFERTAS</a></li>
+ <li><a href='#'>ACERCA DE</a></li>
+ <li><a href='#'>CONTACTO</a></li>
+ </ul>
+ </>
+  )
+}
+
+export default MenuItems
