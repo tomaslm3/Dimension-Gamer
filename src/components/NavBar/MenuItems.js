e6137ddcdc38d39e446e77e874fd80accee60917
Ignacio Castillo <ignacaas@gmail.com>
2022-03-24 18:03:53 -0300

    Arreglo NavBar-MediaQueries

--- src/components/NavBar/MenuItems.js
+++ src/components/NavBar/MenuItems.js
@@ -1,23 +1,31 @@
-
 import React from 'react';
-import {FaRegUser,FaSearch,FaShoppingCart} from "react-icons/fa";
+import {FaRegUser,FaSearch,FaShoppingCart,FaBars} from "react-icons/fa";
  import './NavBar.css';
 
 const MenuItems = () => {
+
+    const mostrarMenu = (e) =>{
+      
+    }
+
+
   return (
  <>
  <div className='contenedor-menu'></div>
  <ul className='menu-items'>
  <li><a href='#'>INICIO</a></li>
  <li><a href='#'>CATALOGO</a></li>
+ <li><a href='#'>NOVEDADES</a></li>
  <li><a href='#'>OFERTAS</a></li>
+ <li><a href='#'>FORO</a></li>
  <li><a href='#'>CONTACTO</a></li>
+ 
  </ul>
 
  <div className='contenedor-btn'>
-        <button><FaSearch/></button>
-        <button><FaRegUser/></button>
+
         <button><FaShoppingCart/></button>
+        <button onClick={mostrarMenu} className="menu-items-btn"><FaBars/></button>
         </div>
  </>
   )
