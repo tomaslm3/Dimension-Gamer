4ef62867592881828e38f58be35f1f622c8475f8
Tom <tomaslm_3@hotmail.co>
2022-03-31 13:25:26 -0300

    Refactorizando logica y estilos nuevos agregados

--- src/components/carrouselFeatures/CarouselFeatures.jsx
+++ src/components/carrouselFeatures/CarouselFeatures.jsx
@@ -1,9 +1,18 @@
 import React, { useState, useEffect } from "react";
 import "./carouselFeatures.css";
+import { FaAngleDoubleLeft, FaAngleDoubleRight } from "react-icons/fa";
 
-function CarouselFeatures({ slides }) {
+function CarouselFeatures({ slides, onSale }) {
   const [currentSlide, setCurrentSlide] = useState(0);
-  const length = slides.length;
+  let slideDiscount = slides.filter(slide => slide.discounts.discount)
+  let length = null
+  // Si trabajamos con el carousel de descuento (onSale) el total del carusel es el total de los juegos con descuento
+  if(onSale){
+    length = slideDiscount.length
+  }else{
+    length = slides.length;
+  }
+
   const autoScroll = true;
   let slideInterval;
   
@@ -16,7 +25,7 @@ function CarouselFeatures({ slides }) {
   };
 
   function auto() {
-    let intervalTime = 5000;
+    let intervalTime = 6000;
     slideInterval = setInterval(nextSlide, intervalTime)
   }
 
@@ -34,32 +43,96 @@ function CarouselFeatures({ slides }) {
     return null;
   }
 
+  // Crousel para las ofertas
+  if(onSale){
   return (
-    <section className="carousel">
+    <div>
+      <div className="title">En oferta</div>
+    <section className="carouselDiscounts">
+      
       <button className="left-arrow" onClick={prevSlide}>
-        Flecha Izquierda
+      <FaAngleDoubleLeft/>
       </button>
-      {slides.map((slide, index) => {
+      {slideDiscount.map((slide, index) => {
         return (
           <div
-            className={index === currentSlide ? "slide active" : "slide"}
+            className={index === currentSlide ? "slide-active" : "slide"}
             key={index}
           >
+            
             {index === currentSlide && (
-              <div>
-                <img src={slide.image} alt="img not found" className="image" />
+              <div className='slideStyles'>
+              <div className="slideInfo">  
                 <h2>{slide.data.title}</h2>
                 <p>{slide.data.description}</p>
+                <p className="offLabel">-{slide.discounts.percent}%</p>
               </div>
+              <img src={slide.image} alt="img not found" className="image" />
+            </div>
             )}
           </div>
         );
       })}
       <button className="right-arrow" onClick={nextSlide}>
-        Flecha Derecha
+      <FaAngleDoubleRight/>
       </button>
     </section>
+    </div>
   );
+  // Crousel para todos los juegos
+  } else {
+    return (
+      <div>
+        <div className="title">Destacados</div> 
+      <section className="carousel">
+        <button className="left-arrow" onClick={prevSlide}>
+        <FaAngleDoubleLeft/>
+        </button>
+        {slides.map((slide, index) => {
+          if (slide.discounts.discount) { // EN caso de que el juego tenga descuento
+          return (
+            <div
+              className={index === currentSlide ? "slide-active" : "slide"}
+              key={index}
+            >
+              {index === currentSlide && (
+                <div className='slideStyles'>
+                  <img src={slide.image} alt="img not found" className="image" />
+                  <div className="slideInfo">  
+                    <h2>{slide.data.title}</h2>
+                    <p>{slide.data.description}</p>
+                    <p>-{slide.discounts.percent}%</p>
+                  </div>
+                </div>
+              )}
+            </div>
+          );
+        } else { // Si el juego no tiene descuento
+          return (
+            <div
+              className={index === currentSlide ? "slide-active" : "slide"}
+              key={index}
+            >
+              {index === currentSlide && (
+                <div className='slideStyles'>
+                  <img src={slide.image} alt="img not found" className="image" />
+                  <div className="slideInfo">
+                    <h2>{slide.data.title}</h2>
+                    <p>{slide.data.description}</p>
+                  </div>
+                </div>
+              )}
+            </div>
+          );
+        }
+        })}
+        <button className="right-arrow" onClick={nextSlide}>
+        <FaAngleDoubleRight/>
+        </button>
+      </section>
+      </div>
+    );
+  }
 }
 
 export default CarouselFeatures;
--- src/containers/app/App.js
+++ src/containers/app/App.js
@@ -3,14 +3,20 @@ import NavBar from '../../components/NavBar/NavBar';
 import { Noticias } from '../../components/noticias/Noticias';
 import './App.css';
 import CarouselFeatures from '../../components/carrouselFeatures/CarouselFeatures';
-import { carrouselData } from '../../components/carrouselFeatures/carouselData';
+import { carouselData } from '../../components/carrouselFeatures/carouselData';
 
 function App() {
   return (
     <>
       <NavBar/>
-      
       <Noticias />
+      <CarouselFeatures 
+      slides={carouselData}
+      />
+      <CarouselFeatures 
+      slides={carouselData}
+      onSale={true}/>
+  
     </>
   );
 }
