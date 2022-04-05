f14076c3a7fe9414556edfdc5284f34b2813b6bf
Tom <tomaslm_3@hotmail.co>
2022-04-04 21:56:21 -0300

    Agregado el import de data para que funcione el carrusel temporalmente

--- src/containers/app/App.js
+++ src/containers/app/App.js
@@ -3,7 +3,7 @@ import NavBar from '../../components/NavBar/NavBar';
 import { Noticias } from '../../components/noticias/Noticias';
 import './App.css';
 import CarouselFeatures from '../../components/carrouselFeatures/CarouselFeatures';
-import { carrouselData } from '../../components/carrouselFeatures/carouselData';
+import { carouselData } from '../../components/carrouselFeatures/carouselData';
 import Portada from '../../components/Portada/Portada';
 
 
